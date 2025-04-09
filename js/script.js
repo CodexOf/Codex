c8a216514ddc971a186725c702b72c950b5931ad
Atollos <codexoflimits@gmail.com>
2025-04-10 02:34:28 +1000

    Copy BITD

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Основная функция инициализации
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Сайт загружен!');
-    
-    // Пример обработчика событий
-    document.querySelector('h1').addEventListener('click', function() {
-        alert('Приветствие!');
-    });
-});
